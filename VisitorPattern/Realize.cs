7010f775855cdeda5a5bd6db181c79e4b45b8dc5
ChasonYun <chasonyun@163.com>
2020-11-09 15:01:19 +0800

    VisitorPattern

---
+++ VisitorPattern/Realize.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    /// <summary>
+    /// 抽象访问者 
+    /// 为每一种类型的元素对象都提供一个访问方法，而访问者可以实现这些访问方法。
+    /// 这些访问方法的命名一般有两种：（1）直接在名称中标明待访问元素对象的具体类型（2）统一 通过参数类型实现重载
+    /// </summary>
+    abstract class Visitor
+    {
+        public abstract void Visit(ConcreteElementA elementA);
+        public abstract void Visit(ConcreteElementB elementB);
+
+        public void Visit(ConcreteElementC elementC)
+        {
+            //elementc的操作代码
+        }
+    }
+
+    /// <summary>
+    /// 这里使用统一的命名实现对不同类型元素的访问操作
+    /// </summary>
+    class ConcreteVisitor : Visitor
+    {
+        public override void Visit(ConcreteElementA elementA)
+        {
+            //元素A的代码
+        }
+
+        public override void Visit(ConcreteElementB elementB)
+        {
+            //元素B的代码
+        }
+    }
+
+    /// <summary>
+    /// 元素类一般定义一个Accept（）方法，用于接收访问者的访问。
+    /// 传入的参数为抽象访问者，即针对抽象编程
+    /// </summary>
+    interface Element
+    {
+        void Accept(Visitor visitor);
+    }
+
+    /// <summary>
+    /// 在具体元素类的Accept()方法中，通过调用Visitor的Visit方法实现对元素的访问，并以当前对象作为Visit方法的参数。
+    /// （1）调用具体元素类的Accept方法。将Visitor子类对象作为其参数
+    /// （2）具体元素类Accept方法内部调用传入的Visitor对象的Visit()方法，例如Visit(ConcreteElementA elementA)，将当前具体元素类对象（this）作为参数
+    /// （3）执行Visitor对象的Visit()方法，在其中还可以调用具体元素对象的业务方法。
+    /// 双重分派机制，使得增加新的访问者无须修改现有的类库代码，只需将新的访问者对象作为参数传入具体元素对象的Accept（）方法，程序运行时将回调在新增Visitor类中定义的Visit方法，
+    /// 从而增加新的元素访问方式。
+    /// </summary>
+    class ConcreteElementA : Element
+    {
+        public void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public void OperationA()
+        {
+            //业务方法
+        }
+    }
+
+    class ConcreteElementB : Element
+    {
+        public void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public void OperationB()
+        {
+
+        }
+    }
+
+    class ConcreteElementC : Element
+    {
+        public void Accept(Visitor visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OperationC()
+        {
+
+        }
+    }
+
+    class ObjectStructure
+    {
+        private List<Element> list = new List<Element>();
+
+        public void Accept(Visitor visitor)
+        {
+            foreach(object obj in list)
+            {
+                ((Element)obj).Accept(visitor);
+            }
+        }
+
+        public void AddElement(Element element)
+        {
+            list.Add(element);
+        }
+
+        public void RemoveElement(Element element)
+        {
+            list.Remove(element);
+        }
+    }
+    class Realize
+    {
+    }
+}
---
+++ VisitorPattern/Sample.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    //财务部 人力部 对员工数据进行汇总
+    //员工包括 正式员工  临时工
+    //
+
+    /// <summary>
+    /// 员工类接口 充当抽象元素类 
+    /// 
+    /// </summary>
+    interface IEmployee
+    {
+        void Accept(Department department);//员工数据接受一个访问者对象的访问
+    }
+
+    /// <summary>
+    /// 全职员工类  具体元素
+    /// </summary>
+    class FulltimeEmployee : IEmployee
+    {
+        string name;
+        double weeklyWage;
+        int workTime;
+
+        public string Name { get => name; set => name = value; }
+        public double WeeklyWage { get => weeklyWage; set => weeklyWage = value; }
+        public int WorkTime { get => workTime; set => workTime = value; }
+
+        public FulltimeEmployee(string name, double weeklyWage, int workTime)
+        {
+            this.name = name;
+            this.weeklyWage = weeklyWage;
+            this.workTime = workTime;
+        }
+        public void Accept(Department department)
+        {
+            department.Visit(this);
+        }
+    }
+
+    class ParttimeEmployee : IEmployee
+    {
+        string name;
+        double hourWage;
+        int workTime;
+
+        public string Name { get => name; set => name = value; }
+        public double HourWage { get => hourWage; set => hourWage = value; }
+        public int WorkTime { get => workTime; set => workTime = value; }
+
+        public ParttimeEmployee(string name,double hourWage,int workTime)
+        {
+            this.name = name;
+            this.hourWage = hourWage;
+            this.workTime = workTime;
+        }
+
+        public void Accept(Department department)
+        {
+            department.Visit(this);
+        }
+    }
+
+    /// <summary>
+    /// 部门类 充当抽象访问者
+    /// </summary>
+    abstract class Department
+    {
+        public abstract void Visit(FulltimeEmployee fulltime);
+        public abstract void Visit(ParttimeEmployee parttime);
+    }
+
+    class FADepartment : Department
+    {
+        /// <summary>
+        /// 财务部对 全职员工的访问
+        /// </summary>
+        /// <param name="fulltime"></param>
+        public override void Visit(FulltimeEmployee fulltime)
+        {
+            int worktime = fulltime.WorkTime;
+            double wage = fulltime.WeeklyWage;
+            if (worktime > 40)
+            {
+                wage = wage + (worktime - 40) * 100;
+            }else if (worktime < 40)
+            {
+                wage = wage - (40 - worktime) * 80;
+            }
+            Console.WriteLine("正式员工{0}的实际工资为：{1}元", fulltime.Name, wage);
+        }
+
+        /// <summary>
+        /// 财务部对 兼职员工的访问
+        /// </summary>
+        /// <param name="parttime"></param>
+        public override void Visit(ParttimeEmployee parttime)
+        {
+            int worktime = parttime.WorkTime;
+            double wage = parttime.HourWage;
+            Console.WriteLine("临时工{0}的实际工资为：{1}元", parttime.Name, worktime * wage);
+        }
+    }
+
+    class HRDepartment : Department
+    {
+        /// <summary>
+        /// 人力资源部对 全职人员的访问
+        /// </summary>
+        /// <param name="fulltime"></param>
+        public override void Visit(FulltimeEmployee fulltime)
+        {
+            int workTime = fulltime.WorkTime;
+            Console.WriteLine("正式员工{0}的实际工时为：{1}", fulltime.Name, fulltime.WorkTime);
+            if (workTime > 40)
+            {
+                Console.WriteLine("正式员工{0}的加班时间为：{1}", fulltime.Name, fulltime.WorkTime - 40);
+            }else if (workTime < 40)
+            {
+                Console.WriteLine("正式员工{0}的请假时间为：{1}", fulltime.Name, fulltime.WorkTime - 40);
+            }
+        }
+
+        /// <summary>
+        /// 全职人员对兼职员工的访问
+        /// </summary>
+        /// <param name="parttime"></param>
+        public override void Visit(ParttimeEmployee parttime)
+        {
+            Console.WriteLine("临时工{0}的实际工时时间为：{1}", parttime.Name, parttime.WorkTime);
+        }
+    }
+
+    class EmployeeList
+    {
+        private List<IEmployee> employees = new List<IEmployee>();
+        public void AddEmployee(IEmployee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public void Accept(Department department)
+        {
+            foreach(IEmployee employee in employees)
+            {
+                employee.Accept(department);
+            }
+        }
+
+        public void RemoveEmployee(IEmployee employee)
+        {
+            employees.Remove(employee);
+        }
+    }
+    class Sample
+    {
+        /// <summary>
+        /// 如果需要增加新的访问者 只需要增加一个新的具体访问者类即可，在该具体访问者棕封装新的操作元素的方法
+        /// 如果需要增加新的具体元素，“返聘人员”，需要在原有的访问者类和具体访问者中增加相应的访问方法
+        /// 对开闭原则的支持具有倾斜性，可以很方便的添加新的访问者，但是添加新的元素较为麻烦
+        /// </summary>
+        public void Test()
+        {
+            EmployeeList employeeList = new EmployeeList();
+            IEmployee ft1, ft2, pt1, pt2;
+            ft1 = new FulltimeEmployee("张无忌", 3200, 45);
+            ft2 = new FulltimeEmployee("杨过", 2000, 40);
+            pt1 = new ParttimeEmployee("段誉", 80, 30);
+            pt2 = new ParttimeEmployee("王语嫣", 200, 20);
+
+            employeeList.AddEmployee(ft1);
+            employeeList.AddEmployee(ft2);
+            employeeList.AddEmployee(pt1);
+            employeeList.AddEmployee(pt2);
+
+            Department department;
+            string departmentType = ConfigurationManager.AppSettings["visitorType"];
+            department = (Department)Assembly.Load("VisitorPattern").CreateInstance(departmentType);//修改配置文件即可实现具体访问者类的更换
+            employeeList.Accept(department);
+            Console.Read();
+        }
+    }
+}
